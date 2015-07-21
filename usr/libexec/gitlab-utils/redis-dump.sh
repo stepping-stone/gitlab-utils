91a4d4abfb22d8083f4d145fcdae0cc04480db9c
Christian Affolter <christian.affolter@stepping-stone.ch>
2015-07-21 18:15:38 +0200

    Added GitLab backup helper scripts.

---
+++ usr/libexec/gitlab-utils/redis-dump.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+################################################################################
+# redis-dump.sh - Creates a new Redis snapshot
+################################################################################
+#
+# Copyright (C) 2015 stepping stone GmbH
+#                    Switzerland
+#                    http://www.stepping-stone.ch
+#                    support@stepping-stone.ch
+#
+# Authors:
+#  Christian Affolter <christian.affolter@stepping-stone.ch>
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public 
+# License as published  by the Free Software Foundation, version
+# 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License  along with this program.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Description:
+# Creates a new Redis database snapshot (RDB) with the BGSAVE command
+# The snapshot will be stored at /var/lib/redis/dump.rdb
+# See: http://redis.io/commands/bgsave and http://redis.io/topics/persistence
+#
+# Usage:
+# redis-dump.sh
+#
+# Example
+# redis-dump.sh
+################################################################################
+
+REDIS_CLI_CMD="/usr/bin/redis-cli"
+REDIS_SOCKET="/run/redis/redis.sock"
+
+if ! test -x "${REDIS_CLI_CMD}"; then
+    echo "Command not found or not executable: '${REDIS_CLI_CMD}'" >&2
+    exit 1
+fi
+
+if ! test -S "${REDIS_SOCKET}"; then
+    echo "Redis socket does not exist: '${REDIS_SOCKET}'" >&2
+    exit 1
+fi
+
+redisCmd="${REDIS_CLI_CMD} -s '${REDIS_SOCKET}' --raw"
+
+# Get the UNIX TIME of the last DB save executed with success
+originalLastSave="$(${redisCmd} LASTSAVE))" 
+lastSave=0
+
+# Issue a background DB save
+${redisCmd} BGSAVE
+
+# Wait until the last DB save timestamp has changed
+until [ ${lastSave} -gt ${originalLastSave} ]; do
+    lastSave="$(${redisCmd} LASTSAVE))"
+    sleep 1
+done
---
+++ usr/libexec/gitlab-utils/zabbix-maintenance-disable.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+################################################################################
+# zabbix-maintenance-disable.sh - Disables Zabbix GitLab backup maintenance mode
+################################################################################
+#
+# Copyright (C) 2015 stepping stone GmbH
+#                    Switzerland
+#                    http://www.stepping-stone.ch
+#                    support@stepping-stone.ch
+#
+# Authors:
+#  Christian Affolter <christian.affolter@stepping-stone.ch>
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public 
+# License as published  by the Free Software Foundation, version
+# 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License  along with this program.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Description:
+# Disables the "Zabbix maintenance mode trigger" for the services unicorn and
+# sidekiq by sennding a Zabbix trapper message for the following keys:
+#  - sst.maintenance.status[unicorn] = 0
+#  - sst.maintenance.status[sidekiq] = 0
+#
+# Usage:
+# zabbix-maintenance-disable.sh
+#
+# Example:
+# zabbix-maintenance-disable.sh
+################################################################################
+ZABBIX_SENDER_CMD="/usr/bin/zabbix_sender"
+
+if ! test -x "${ZABBIX_SENDER_CMD}"; then
+    echo "Command not found or not executable: '${ZABBIX_SENDER_CMD}'" >&2
+    exit 1
+fi
+
+
+${ZABBIX_SENDER_CMD} --config /etc/zabbix/zabbix_agentd.conf \
+                     --key 'sst.maintenance.status[unicorn]' \
+                     --value 1 \
+                     --verbose
+
+${ZABBIX_SENDER_CMD} --config /etc/zabbix/zabbix_agentd.conf \
+                     --key 'sst.maintenance.status[sidekiq]' \
+                     --value 1 \
+                     --verbose
