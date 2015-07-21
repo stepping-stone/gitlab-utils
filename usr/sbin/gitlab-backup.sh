4c4560a20a479c6d6e1a229017f089d4112634e7
Christian Affolter <christian.affolter@stepping-stone.ch>
2015-07-21 17:26:51 +0200

    Initial configuration and config parsing for gitlab-backup.sh

--- usr/sbin/gitlab-backup.sh
+++ usr/sbin/gitlab-backup.sh
@@ -35,3 +35,210 @@
 # Example:
 # gitlab-backup.sh <EXAMPLE-OPTIONS-AND-ARGUMENTS>
 ################################################################################
+
+# Root directory of the GitLab utils
+ROOT_DIR="$(dirname $(readlink -f ${0}))/../.."
+
+# bash lib directory
+LIB_DIR="${ROOT_DIR}/usr/share/stepping-stone/lib/bash"
+
+# Required external commands
+GETENT_CMD="/usr/bin/getent"
+REDIS_CLI_CMD="/usr/bin/redis-cli"
+
+source "${LIB_DIR}/input-output.lib.sh"
+source "${LIB_DIR}/config.lib.sh"
+source "${LIB_DIR}/validation.lib.sh"
+source "${LIB_DIR}/input-validation.lib.sh"
+
+
+# Set restrictive umask by default
+umask 0077
+
+# Patterns that match no files shall expand to zero arguments, rather than to
+# themselves.
+shopt -s nullglob
+
+
+##
+# Private variables, do not overwrite them
+#
+# Default configuration file
+_DEFAULT_CONFIG_FILE="${ROOT_DIR}/etc/gitlab-backup.conf"
+
+
+# Process all arguments passed to this script
+#
+# processArguments
+function processArguments ()
+{
+    # Define all options as unset by default
+    declare -A optionFlags
+
+    for optionName in a c d h i; do
+        optionFlags[${optionName}]=false
+    done
+
+    while getopts ":c:dhi:" option; do
+        debug "Processing option '${option}'"
+
+        case $option in
+            c )
+                configFile="${OPTARG}"
+                debug "Using config file: ${configFile}"
+            ;;
+
+            d )
+                export DEBUG="yes"
+                debug "Enabling debug messages"
+            ;;
+
+            h )
+                printUsage
+                exit 0
+            ;;
+
+            \? )
+                error "Invalid option '-${OPTARG}' specified"
+                printUsage
+                exit 1
+            ;;
+
+            : )
+                error "Missing argument for '-${OPTARG}'"
+                printUsage
+                exit 1
+            ;;
+        esac
+
+        optionFlags[${option}]=true # Option was provided
+    done
+
+    # Set static action, as there are currently no other ones
+    action="backup"
+
+    if ! ${optionFlags[c]}; then
+        debug "No config file was provided, use the default one"
+        configFile="${_DEFAULT_CONFIG_FILE}"
+    fi
+}
+
+
+# Displays the help message
+#
+# printUsage
+function printUsage ()
+{
+    cat << EOF
+
+Usage: $( ${BASENAME_CMD} "$0" ) [OPTION]...
+
+    -c CONFIGFILE   Specifies the configuration file to use, which defaults to
+                    $(realpath -m ${_DEFAULT_CONFIG_FILE})
+
+    -d              Enable debug messages
+    -h              Display this help and exit
+EOF
+}
+
+
+# Load and check the configuration values
+#
+# loadAndCheckConfig CONFIGFILE
+function loadAndCheckConfig ()
+{
+    configLoadConfig "$1"
+
+    local configParameters='gitLabUtilsMaintenanceModeActive
+                            gitLabUtilsMaintenanceModeEnableScript
+                            gitLabUtilsMaintenanceModeDisableScript
+                            gitLabUtilsMaintenanceModeUser
+                            gitLabUtilsServiceShutdown
+                            gitLabUtilsServiceShutdownStopScript
+                            gitLabUtilsServiceShutdownStartScript
+                            gitLabUtilsServiceShutdownUser
+                            gitLabUtilsRdbmsDump
+                            gitLabUtilsRdbmsDumpScript
+                            gitLabUtilsRdbmsDumpUser
+                            gitLabUtilsFileBackup
+                            gitLabUtilsFileBackupScript
+                            gitLabUtilsFileBackupUser
+                            gitLabUtilsNoSqlDump
+                            gitLabUtilsNoSqlDumpScript
+                            gitLabUtilsNoSqlDumpUser
+                           '
+
+    local parameter=''
+    for parameter in ${configParameters}; do
+        configDieIfValueNotPresent "$parameter"
+    done
+
+    local script=''
+    for script in ${gitLabUtilsMaintenanceModeEnableScript} \
+                  ${gitLabUtilsMaintenanceModeDisableScript} \
+                  ${gitLabUtilsServiceShutdownStopScript} \
+                  ${gitLabUtilsServiceShutdownStartScript} \
+                  ${gitLabUtilsRdbmsDumpScript} \
+                  ${gitLabUtilsFileBackupScript} \
+                  ${gitLabUtilsNoSqlDumpScript}
+    do
+        # Passing ${script} unquoted was intentional to split of the possible
+        # script arguments.
+        validationDieIfCommandMissing ${script}
+    done
+
+    local user
+    for user in ${gitLabUtilsMaintenanceModeUser} \
+                ${gitLabUtilsServiceShutdownUser} \
+                ${gitLabUtilsRdbmsDumpUser} \
+                ${gitLabUtilsFileBackupUser} \
+                ${gitLabUtilsNoSqlDumpUser};
+    do
+        if ! ${GETENT_CMD} passwd "${user}" > /dev/null; then
+            die "User '$user' does not exist"
+        fi
+    done
+}
+
+
+# Checks the local environment
+#
+# Ensures that all external commands are available
+#
+# checkEnvironment
+function checkEnvironment ()
+{
+    local cmd=''
+    for cmd in ${GETENT_CMD} \
+               ${REDIS_CMD}
+    do
+        validationDieIfCommandMissing "${cmd}"
+    done
+}
+
+
+# The main function of this script
+#
+# Processes the passed command line options and arguments,
+# loads and validates the configuration and calls the action.
+#
+# main $@
+function main ()
+{
+    processArguments $@
+    loadAndCheckConfig "${configFile}"
+    checkEnvironment
+
+    # Uppercase the first letter of the action name and call the function
+    action$(action^)
+
+    return $?
+}
+
+function actionBackup ()
+{
+    return true
+}
+
+
+main
